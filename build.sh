8728dfc18cd0d21c9d5e815d74024cb7d2367127
roman shushakov <roman.a.shushakov1@gmail.com>
2023-06-14 08:13:40 +0200

    Updated dependencies in GOF module

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cd ./wasm_modules/game_of_life && \
-RUSTFLAGS=--cfg=web_sys_unstable_apis cargo build --release --no-default-features --target wasm32-unknown-unknown && \
+cargo build --release --no-default-features --target wasm32-unknown-unknown && \
 wasm-bindgen --target web --out-name game_of_life --out-dir ../../wasm --no-typescript ./target/wasm32-unknown-unknown/release/game_of_life.wasm
--- build_worker.sh
+++ build_worker.sh
@@ -1,3 +1,3 @@
 cd ./wasm_modules/game_of_life && \
-RUSTFLAGS=--cfg=web_sys_unstable_apis cargo build --release --no-default-features --target wasm32-unknown-unknown && \
+cargo build --release --no-default-features --target wasm32-unknown-unknown && \
 wasm-bindgen --target no-modules --out-name game_of_life_worker --out-dir ../../wasm --no-typescript ./target/wasm32-unknown-unknown/release/game_of_life.wasm
